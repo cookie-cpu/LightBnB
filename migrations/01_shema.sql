72a93caf167f999640d6671c1626250f47ec5575
Matthew Cook <matthewcook13987@gmail.com>
2021-03-03 01:27:48 +0000

    Began implementation of create table queries for lightbnb

---
+++ migrations/01_shema.sql
@@ -0,0 +1,16 @@
+DROP TABLE users;
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255),
+  email VARCHAR(255),
+  password VARCHAR(255)
+);
+
+CREATE TABLE reservations (
+  id SERIAL PRIMARY KEY NOT NULL,
+  start_date DATE,
+  end_date DATE,
+  property_id INTEGER REFERENCES properties.id,
+  guest_id INTEGER REFERENCES users.id
+)
