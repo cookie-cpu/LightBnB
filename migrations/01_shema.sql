03e36f2d26ceffdbd91af48c1c128599d63e84d2
Matthew Cook <matthewcook13987@gmail.com>
2021-03-03 20:20:07 +0000

    Finished writing create table sequence to initialize the database with linked tables.

--- migrations/01_shema.sql
+++ migrations/01_shema.sql
@@ -1,4 +1,8 @@
-DROP TABLE users;
+DROP TABLE users IF EXISTS;
+DROP TABLE reservations IF EXISTS;
+DROP TABLE property_reviews IF EXISTS;
+DROP TABLE properties IF EXISTS;
+
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
@@ -7,10 +11,39 @@ CREATE TABLE users (
   password VARCHAR(255)
 );
 
+CREATE TABLE properties (
+  id SERIAL PRIMARY KEY NOT NULL,
+  owner_id INTEGER REFERENCES users.id,
+  title VARCHAR(255),
+  description TEXT,
+  thumbnail_photo_url VARCHAR(255),
+  cover_photo_url VARCHAR(255),
+  cost_per_night INTEGER,
+  parking_spaces INTEGER,
+  number_of_bathrooms INTEGER,
+  number_of_bedrooms INTEGER,
+  country VARCHAR(255),
+  street VARCHAR(255),
+  city VARCHAR(255),
+  province VARCHAR(255),
+  postal_code VARCHAR(255),
+  active BOOLEAN
+);
+
 CREATE TABLE reservations (
   id SERIAL PRIMARY KEY NOT NULL,
   start_date DATE,
   end_date DATE,
   property_id INTEGER REFERENCES properties.id,
   guest_id INTEGER REFERENCES users.id
-)
+);
+
+CREATE TABLE property_reviews (
+  id SERIAL PRIMARY KEY NOT NULL,
+  guest_id INTEGER REFERENCES users.id,
+  property_id INTEGER REFERENCES properties.id,
+  reservation_id INTEGER REFERENCES reservations.id,
+  rating SMALLINT,
+  message TEXT
+);
+
