3e36e5085e7fdb6b295bf33192f0d5f036d258c1
Matthew Cook <matthewcook13987@gmail.com>
2021-03-04 02:20:08 +0000

    Wrote a query for addUser to write new users to the SQL database.

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -85,10 +85,29 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+
+  return pool.query(`
+
+  INSERT INTO users (name, password, email)
+  VALUES ($1, $2, $3)
+  RETURNING *;
+  `,[user.name, user.email, user.password])
+  .then( res => {
+    console.log(res.rows)
+    if (res.rows){
+      return res.rows[0];
+    } else {
+      return null;
+    }
+  })
+  .catch(err => {
+    console.log(err)
+  })
+
+  // const userId = Object.keys(users).length + 1;
+  // user.id = userId;
+  // users[userId] = user;
+  // return Promise.resolve(user);
 }
 exports.addUser = addUser;
 
