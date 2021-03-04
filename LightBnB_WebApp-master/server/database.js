aa8691c07a15eb4d269e780fbab356790b4029c2
Matthew Cook <matthewcook13987@gmail.com>
2021-03-04 02:03:51 +0000

    Refactored getUserWithId using code from getUserEmail to pull from the database.

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -49,7 +49,22 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+
+  return pool.query(`
+  SELECT * FROM users
+  WHERE id = $1;
+  `, [id])
+  .then( res => {
+    console.log(res.rows)
+  })
+  .catch(err => {
+    console.log(err)
+  })
+
+
+
+
+  // return Promise.resolve(users[id]);
 }
 exports.getUserWithId = getUserWithId;
 
